aad7dcd0666517042b3b026eae1ff51fe8903bec
zed1025 <amit251098@yahoo.in>
2020-07-27 14:44:39 +0530

    chapter3-4-blog api frontend and backend

---
+++ blogapi/posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from posts.serializers import PostSerializer
+from posts.models import Post
+
+# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
