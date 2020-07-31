4c9fda031ada5cf4bce742d044250f75dd568e4f
zed1025 <amit251098@yahoo.in>
2020-07-31 23:19:40 +0530

    chapter9-not complete

--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -2,10 +2,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
+schema_view = get_schema_view(title='Blog API')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('docs/', include_docs_urls(title='Blog API')),
+    path('schema/', schema_view),
 ]
